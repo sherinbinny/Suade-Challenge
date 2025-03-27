1f2fbdb23d436f849925b0fd57d3fd98c2f5328c
Sherin Binny <sherinbinny@gmail.com>
2025-03-27 12:12:00 +0000

    exercise 5

--- exercise5/src/App.vue
+++ exercise5/src/App.vue
@@ -1,35 +1,35 @@
 <template>
+  <div class="container">
+    <h2>Exercise 5</h2>
+    <h4>Statement</h4>
+    <div class="statement">
+      <p>
+        Please implement the 2 empty functions from <span class="code">helpers/helpers.js</span>: <span class="code">getValueAtPath</span> and
+        <span class="code">groupByKey</span>.
+      </p>
+      <p>
+        Check their usage in <span class="code">App.js</span> to understand the specs.
+      </p>
+    </div>
+    <h4>Solution</h4>
+    <div class="solution">
 
-  <h2>Exercise 5</h2>
-  <h4>Statement</h4>
-  <div class="statement">
-    <p>
-      Please implement the 2 empty functions from <span class="code">helpers/helpers.js</span>: <span class="code">getValueAtPath</span> and
-      <span class="code">groupByKey</span>.
-    </p>
-    <p>
-      Check their usage in <span class="code">App.js</span> to understand the specs.
-    </p>
-  </div>
-  <h4>Solution</h4>
-  <div class="solution">
+      <h5>Gender</h5>
+      <p v-if="genderGroups">Male: {{ parseInt(genderGroups.male * 100) }}%, Female: {{ parseInt(genderGroups.female * 100) }}%</p>
 
-    <h5>Gender</h5>
-    <p v-if="genderGroups">Male: {{ parseInt(genderGroups.male * 100) }}%, Female: {{ parseInt(genderGroups.female * 100) }}%</p>
-
-    <h5>Table</h5>
-    <table>
-      <thead>
-        <tr>
-          <th v-for="col in columns" :key="col">{{ col.split('.').slice(-1)[0] }}</th>
+      <h5>Table</h5>
+      <table>
+        <thead>
+          <tr>
+            <th v-for="col in columns" :key="col">{{ col.split('.').slice(-1)[0] }}</th>
+          </tr>
+        </thead>
+        <tr v-for="item in people" :key="item._id">
+          <th v-for="col in columns" :key="col">{{ getValueAtPath(item, col) }}</th>
         </tr>
-      </thead>
-      <tr v-for="item in people" :key="item._id">
-        <th v-for="col in columns" :key="col">{{ getValueAtPath(item, col) }}</th>
-      </tr>
-    </table>
+      </table>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -63,17 +63,96 @@
 
 <style lang="scss">
 
-.statement, .solution {
+.statement, .solution
+{
   border: 1px solid #dedede;
   padding: 0 0.5em;
   min-height: 40px;
 
-  .code {
+  .code
+  {
     color: #434343;
     background-color: #f9f9f9;
     padding: 0 5px;
   }
 }
 
+.container
+{
+  padding: 20px;
+  margin: 0 auto;
+  font-family: Arial, sans-serif;
+  background-color: #f9f9f9;
+}
+
+h2,
+h4,
+h5
+{
+  color: #333;
+}
+
+h2
+{
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+h4
+{
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+}
+
+h5
+{
+  font-size: 1.3rem;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+
+p
+{
+  font-size: 1rem;
+  color: #555;
+}
+
+table
+{
+  width: 100%;
+  border-collapse: collapse;
+  margin: 15px 0;
+  font-size: 1rem;
+  text-transform: capitalize;
+}
+
+th,
+td
+{
+  border: 1px solid #ddd;
+  padding: 12px;
+  text-align: left;
+}
+
+th
+{
+  color: #333;
+}
+
+thead th
+{
+  background-color: #e0e0e0;
+}
+
+tr:nth-child(even)
+{
+  background-color: #f4f4f4;
+}
+
+tr:nth-child(odd)
+{
+  background-color: #f9f9f9;
+}
 
 </style>
--- exercise5/src/helpers/helpers.js
+++ exercise5/src/helpers/helpers.js
@@ -1,12 +1,28 @@
 export default {
   // group an array by key and returns an object containing percentage for each group
   // ex: groupByKey([{color: 'blue'}, {color: 'green'}, {color: 'blue'}, {color: 'blue'}], 'color')  =>  {blue: 0.75, green: 0.25}
-  groupByKey(array, key) {
+  groupByKey(array, key)
+  {
     // TODO: implement logic here
+    const total = array.length;
+    const counts = array.reduce((acc, obj) => 
+    {
+      const keyValue = obj[key];
+      acc[keyValue] = (acc[keyValue] || 0) + 1;
+      return acc;
+    }, {});
+
+    // Convert counts to percentages
+    return Object.keys(counts).reduce((acc, key) => {
+      acc[key] = counts[key] / total;
+      return acc;
+    }, {});
   },
   // get the value of an object at a given dotted path
   // ex: getValueAtPath({my: {dotted: {path: 123}}}, 'my.dotted.path')  =>  123
-  getValueAtPath(obj, path, defaultValue='none') {
+  getValueAtPath(obj, path, defaultValue='none')
+  {
     // TODO: implement logic here
+    return path.split('.').reduce((acc, part) => acc && acc[part], obj) || defaultValue;
   },
 };
